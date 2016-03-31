ce728585b211fccb8e70221a8daea531ef13329d
Tu Hoang <hatu87@gmail.com>
2016-03-31 20:11:03 +0700

    Finish milestone 4

---
+++ app/assets/javascripts/socket_chat.js
@@ -0,0 +1,26 @@
+// file: app/assets/javascript/socket_chat.js
+// please type it yourself, do NOT copy & paste
+
+window.ws = new WebSocket("ws://" + window.document.location.host)
+
+ws.onmessage = function(rawMessage) {
+  var message = JSON.parse(rawMessage.data);
+  $(".messages").append($('<div />').html(message.user.email + ': ' + message.content));
+};
+
+function setupForm() {
+  $("form#new_message").on("submit", function(event) {
+    event.preventDefault();
+    var content = $("#message_content").val();
+    var user    = window.currentUser;
+    // let's use JSON to convert an object into string
+    ws.send(JSON.stringify({ content: content, user: user }));
+    // clear the input text:
+    $("#message_content").val('')
+  });
+}
+
+// use page:change to be compatible with turbolinks
+$(document).on("page:change", function(){
+  setupForm();
+});
